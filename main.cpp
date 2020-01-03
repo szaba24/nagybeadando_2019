ec24db60b8f64f51bddbfc86092c324d78868f1b
leviboy2000 <57921897+leviboy2000@users.noreply.github.com>
2020-01-03 20:01:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,11 @@
 #include <set>
 #include <queue>
 #include <string>
+#include <list>
 
 
 using namespace std;
 
-
-struct kocsi
-{
-    string azonosito;
-    int kapacitas = 0;
-    string kezdo_allomas;
-};
-
-
-struct menetrend
-{
-    pair<string,int> menetrendek;
-};
 
 
 struct termek
@@ -32,13 +20,90 @@ struct termek
     int meret;
 };
 
+struct kocsi ///vonathoz csatlakoztatható kocsi
+{
+    string azonosito;
+    int kapacitas = 0;
+    string kezdo_allomas;
+    list<termek> tartalma; ///mi van belepakolva
+};
 
+
+struct vonat ///vonat hova megy
+{
+    string id;
+    int kapacitas;
+    map<string,int> allomasok; /// allomas neve es hogy mikor van ott
+    list<kocsi> csatlakoztatott_kocsik;
+};
+
+
+
+
+void pakol(kocsi &k, termek &t, int &m)
+{
+    if(m<=(k.kapacitas-k.tartalma.size()))
+    {
+        for(int i=0; i<m; i++)
+        {
+            k.tartalma.push_back(t);
+        }
+    }
+    else
+    {
+        cout << "Nincs eleg hely a bepakolashoz! :(" << endl;
+    }
+}
+
+void kipakol(kocsi &k, termek &t, int &m)
+{
+    int kipakoltak_szama =0;
+    list<termek>::iterator it = k.tartalma.begin();
+    while(it!=k.tartalma.end() && kipakoltak_szama <m)
+    {
+        if(it->id == t.id)
+        {
+            k.tartalma.erase(it);
+            kipakoltak_szama++;
+        }
+        else
+        {
+            it++;
+        }
+    }
+
+}
+
+void csatol(vonat &v, kocsi &k)
+{
+    if (v.csatlakoztatott_kocsik.size() < v.kapacitas)
+    {
+        v.csatlakoztatott_kocsik.push_back(k);
+    }
+    else
+    {
+        cout << "Nem lehet tobb kocsit csatlakoztatni! :(" << endl;
+    }
+}
+
+void lecsatol(vonat &v, kocsi &k)
+{
+    list<kocsi>::iterator it = v.csatlakoztatott_kocsik.begin();
+    while(it != v.csatlakoztatott_kocsik.end())
+    {
+        if(it->azonosito == k.azonosito)
+        {
+            v.csatlakoztatott_kocsik.erase(it);
+            break;
+        }
+    }
+}
 
 
 int main()
 {
     kocsi k;
-    menetrend m;
+    vonat v;
     termek t;
 
 
@@ -112,6 +177,15 @@ int main()
         cout << "az arukat tartalmazo fajlt nem sikerult megnyitni";
 
     }
+
+    int mennyiseg=0; ///mennyi cuccot akarunk bepakolni
+    pakol(k,t,mennyiseg); ///kell egy kocsi, egy termék és hogy mennyit rakjunk bele
+
+    kipakol(k,t,mennyiseg);
+
+    csatol(v, k);
+
+    lecsatol(v,k);
 
     return 0;
 }
